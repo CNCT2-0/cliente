8f982ff7492c92b8b4595814806247dfa3a0cb50
nikolasHrd <nukoluks@hotmail.com>
2022-11-27 15:39:59 -0500

    noveno

---
+++ clienteServices.js
@@ -0,0 +1,50 @@
+const getMongo = require("./mongodb.js")
+
+async function getConexiones() {
+    const nameDB = "cnct"
+    const client = await getMongo.getClientExport(nameDB)
+    const collection = await getMongo.getCollectionExport(client, nameDB)
+    return { collection, client }
+}
+
+const clientesGet = () =>{
+    return Clientes
+}
+
+const clientesGetId = async (id) =>{
+    const { collection, client } = await getConexiones()
+    var clienteEncontrado = null
+    await collection.findOne({"_id":id}).then(
+        (Clientes)=>{
+            return clienteEncontrado = Clientes
+        }
+    )
+    await getMongo.closeClientExport(client)
+    return clienteEncontrado
+}
+
+//********************* SET ************************
+
+const clientesSet = (cliente) =>{
+    Clientes.push(cliente)
+    return Clientes
+
+}
+
+//********************* DELETE ************************
+
+const clientesDelete = (id) =>{
+    cliente = Clientes.filter((carrito)=>{
+        return carrito.id != id
+    }
+    )
+    console.log(cliente)
+    return cliente
+}
+
+
+
+module.exports.clientesGetExport = clientesGet;
+module.exports.clientesSetExport = clientesSet;
+module.exports.clientesDeleteExport = clientesDelete;
+module.exports.clientesGetIdExport = clientesGetId;
